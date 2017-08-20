78582d32e3b67673677e816831bcbae1983a5ec1
relyativus <relyativus@gmail.com>
2017-08-20 19:41:13 +0300

    Use bean lifecycle

--- src/main/java/co/inventorsoft/spring/service/logger/Logger.java
+++ src/main/java/co/inventorsoft/spring/service/logger/Logger.java
@@ -4,12 +4,16 @@ import co.inventorsoft.spring.model.base.Exportable;
 import co.inventorsoft.spring.model.base.FormattedDataBuilder;
 import co.inventorsoft.spring.model.base.FormattedDataBuilderFactory;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class Logger {
 
@@ -17,10 +21,12 @@ public class Logger {
 
     private FormattedDataBuilderFactory formattedDataBuilderFactory;
 
+    private String directory;
+
     public Logger(@Value("${application.logger.directory}") final String directory,
                   final FormattedDataBuilderFactory formattedDataBuilderFactory) {
+        this.directory = directory;
         this.formattedDataBuilderFactory = formattedDataBuilderFactory;
-        openLogFile(directory);
     }
 
     @SneakyThrows
@@ -32,16 +38,20 @@ public class Logger {
         fos.flush();
     }
 
+    @PreDestroy
     @SneakyThrows
     public void closeConnection() {
+        log.info("Closing system monitor file ...");
         fos.close();
     }
 
-    private void openLogFile(String directory) {
+    @PostConstruct
+    private void openLogFile() {
+        log.info("Initializing system monitor files under {} directory", directory);
         try {
             fos = new FileOutputStream(directory + "\\output.log", true);
         } catch (IOException e) {
-            System.out.println("Cannot create file for logs. Reason: " + e.getMessage());
+            log.warn("Cannot create file for logs. Reason: ", e);
         }
     }
 }
